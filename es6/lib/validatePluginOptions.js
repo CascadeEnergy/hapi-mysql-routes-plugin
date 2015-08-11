660849c0eda83836a426d5f92c3037ac4deb7f37
jinsyaoommen <jinsyaoommen@gmail.com>
2015-08-10 21:26:55 -0700

    SEN-1612 -- validate required options for plugin

--- es6/lib/validatePluginOptions.js
+++ es6/lib/validatePluginOptions.js
@@ -1,14 +1,12 @@
 'use strict';
 
-import Boom from 'boom';
-
 const requiredOptions = [ 'mysqlConfig', 'tableName', 'tableIndex' ];
 
 function validatePluginOptions(options) {
 
   function validate(value) {
     if (options[value] === undefined) {
-      throw Boom.create(500, `${value} is a required plugin option`);
+      throw new Error(`${value} is a required plugin option`);
     }
   }
 
